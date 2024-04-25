46ec20eba566df9918dcea67da72c99cb4131362
SaLuTeK1 <138734652+SaLuTeK1@users.noreply.github.com>
2024-04-25 23:56:43 +0300

    1

--- main.py
+++ main.py
@@ -1,61 +1,111 @@
-# 1)написати функцію на замикання котра буде в собі зберігати список справ, вам потрібно реалізувати два методи:
-# - перший записує в список нову справу
-# - другий повертає всі записи
+import turtle
+import time
 
-from typing import Callable
+# Побудова фонового статичного рисунка
+def draw_background():
+    screen = turtle.Screen()
+    screen.setup(width=800, height=600)
+    screen.bgcolor("skyblue")
+
+    # Намалюємо небо
+    sky = turtle.Turtle()
+    sky.penup()
+    sky.goto(-400, -100)
+    sky.pendown()
+    sky.color("lightblue")
+    sky.begin_fill()
+    sky.goto(400, -100)
+    sky.goto(400, 600)
+    sky.goto(-400, 600)
+    sky.goto(-400, -100)
+    sky.end_fill()
+
+    # Намалюємо землю
+    ground = turtle.Turtle()
+    ground.penup()
+    ground.goto(-400, -400)
+    ground.pendown()
+    ground.color("burlywood4")
+    ground.begin_fill()
+    ground.goto(400, -400)
+    ground.goto(400, -100)
+    ground.goto(-400, -100)
+    ground.goto(-400, -400)
+    ground.end_fill()
+
+    # Намалюємо траву
+    grass = turtle.Turtle()
+    grass.penup()
+    grass.goto(-400, -110)
+    grass.pendown()
+    grass.color("green")
+    grass.begin_fill()
+    grass.goto(400, -110)
+    grass.goto(400, -90)
+    grass.goto(-400, -90)
+    grass.goto(-400, -110)
+    grass.end_fill()
+
+    # Намалюємо хмари
+    cloud(-300,200,20)
+    cloud(270,250,20)
+    cloud(100,180,20)
+    cloud(-90,230,20)
+
+    # Відключимо анімацію для сповільнення
+    turtle.tracer(0)
+
+# Функція для малювання хмар
+def filled_circle(radius, color):
+    turtle.color(color,color)
+    turtle.begin_fill()
+    turtle.circle(radius)
+    turtle.end_fill()
+def cloud(x,y, radius, cloud_color="white"):
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+
+    filled_circle(radius,cloud_color)
+    turtle.forward(radius)
+    filled_circle(radius,cloud_color)
+    turtle.right(90)
+    filled_circle(radius,cloud_color)
+    turtle.right(90)
+    filled_circle(radius,cloud_color)
+    turtle.right(90)
+    filled_circle(radius,cloud_color)
+    turtle.right(90)
 
 
-def notebook() -> tuple[Callable[[str], None], Callable[[], list[str]]]:
-    todo_list: list[str] = []
+# Анімація запуску ракети
+def launch_rocket(thrust, mass):
+    # Створення ракети
+    rocket = turtle.Turtle()
+    rocket.shape("triangle")
+    rocket.color("red")
+    rocket.penup()
+    rocket.goto(0, -90)
 
-    def add_todo(todo: str) -> None:
-        nonlocal todo_list
-        todo_list.append(todo)
+    # Розвернемо трикутник для рівнобедренного вигляду
+    rocket.setheading(90)  # Змінюємо кут нахилу трикутника на 90 градусів
 
-    def get_all() -> list[str]:
-        nonlocal todo_list
-        return todo_list.copy()
+    # Включимо анімацію
+    turtle.tracer(1)
 
-    return add_todo, get_all
+    # Симуляція руху ракети
+    for _ in range(120):
+        rocket.sety(rocket.ycor() + 2 * (thrust / mass))  # Збільшуємо y-координату з урахуванням сили тяги та маси
+        time.sleep(0.1)
 
+    turtle.done()
 
-add, get = notebook()
-add('lox')
-print(get())
+# Інтерактивна взаємодія
+def interactive_launch():
+    thrust = float(input("Введіть силу тяги двигуна: "))
+    mass = float(input("Введіть масу ракети: "))
+    draw_background()
+    launch_rocket(thrust, mass)
 
-
-#
-# 3) створити функцію котра буде повертати сумму розрядів числа у вигляді строки (також використовуемо типізацію)
-# Приклад:
-# expanded_form(12) # return '10 + 2'
-# expanded_form(42) # return '40 + 2'
-# expanded_form(70304) # return '70000 + 300 + 4'
-def expanded_form(num: int):
-    st = str(num)
-    list = []
-    size = len(st)-1
-    for i ,char in enumerate(st):
-        if char != '0':
-            list.append(char+'0'*(size-i))
-    return ' + '.join(list)
-
-print(expanded_form(27197))
-
-def decor(func):
-    i = 0
-    def inner(*args, **kwargs):
-        nonlocal i
-        i += 1
-        print('*'*20)
-        print(i)
-        func(*args, **kwargs)
-        print('*'*20)
-    return inner
-
-@decor
-def func1():
-    print('func1')
-
-func1()
-func1()
-func1()
+# Виклик функції для інтерактивного запуску ракети
+interactive_launch()
